b2e61f6dc9acf1dd140d8d223db146e2cceb8a12
Libor Knappek <libor.knappek@gmail.com>
2021-04-02 18:56:02 +0200

    Finla plot

--- ViewDb.py
+++ ViewDb.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import json
 import os
+from datetime import datetime
 
 #get db from HEROKU into DataFrame
 #load config
@@ -30,28 +31,38 @@ with engine.connect() as conn:
 df = pd.DataFrame(data = result, columns = ["item_date", "consumption","ma7"])
 df["item_date"] = pd.to_datetime(df["item_date"])
 df = df.sort_values("item_date")
+#Create DF with top 5 values
 dfMax = df.sort_values("consumption", ascending=False)[:5]
-print(dfMax)
+#Create DF with last 30 values, ascending
+df30 = df[-30:]
+
+
+myData = []
+for val in dfMax[["item_date","consumption"]].itertuples():
+    myData.append([val[1].strftime('%d.%m.%Y'),val[2]])
+
+
 
 fig = plt.figure(figsize=(18,8))
 gs = fig.add_gridspec(2,2)
 ax1 = fig.add_subplot(gs[0,:])
-ax1.plot(df["item_date"], df["consumption"])
-ax1.plot(df["item_date"], df["ma7"])
+ax1.plot(df["item_date"], df["consumption"], label="Spotřeba")
+ax1.plot(df["item_date"], df["ma7"], label="7denní průměr")
 ax1.grid(axis="y", lw=0.5)
-ax1.set_title("Denní Spotřeba (kWh")
+ax1.set_xlabel("Datum")
+ax1.set_ylabel("kWh/den")
+ax1.set_title("Denní Spotřeba (kWh)")
+ax1.legend(loc=(0.01,0.78))
 ax2 = fig.add_subplot(gs[1,0])
-ax2.bar(range(0, len(dfMax.index)),height=dfMax["consumption"])
-ax2.set_xticklabels(dfMax["item_date"])
-ax2.set_title("TOP 5 Spotřeba")
+ax2.plot(df30["item_date"], df30["consumption"],color="green", label="Spotřeba")
+ax2.plot(df30["item_date"], df30["ma7"], color="red", label="7denní průměr")
+ax2.grid(axis="y", lw=0.5)
+ax2.set_title("Spotřeba posledních 30 dní")
+ax2.legend(loc=(0.8,0.8))
+ax3 = fig.add_subplot(gs[1,1])
+ax3.axis("tight")
+ax3.axis("off")
+ax3.table(cellText=myData, colLabels=["Date","Spotřeba (kWh)"], colLoc="right", colColours=["#3399FF","#3399FF"], loc='center')
+ax3.set_title("TOP 5 Spotřeba")
 plt.tight_layout()
 plt.show()
-
-# fig, ax = plt.subplots(figsize=(24,8)) # simplest way to create fig obj and axes
-# ax.plot(df["item_date"], df["consumption"])
-# ax.plot(df["item_date"], df["ma7"])
-# ax.set_xlabel("Date")
-# plt.title("Denní spotřeba (kWh)")
-# plt.grid(axis="y", lw=0.5)
-# plt.tight_layout()
-# plt.show()
