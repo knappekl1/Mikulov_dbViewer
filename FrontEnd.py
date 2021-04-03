96ca5535d84c544937535605aae757fa40aaddce
Libor Knappek <libor.knappek@gmail.com>
2021-04-03 16:06:46 +0200

    v1.3

--- FrontEnd.py
+++ FrontEnd.py
@@ -1,3 +1,31 @@
 import PySimpleGUI as sg
+import ViewDb
 
+dataDict = ViewDb.getDbData()
+lastDbDate = dataDict["maxDate"][0][0].strftime("%d.%m.%Y")
 # Start up screen
+# Define the window's contents
+layout = [[sg.Text("Spotřeba Mikulov", font=1, size=(100,1), border_width=15)],
+            [sg.Text("Poslední měřené datum: " + lastDbDate,size=(100,1), border_width=15)],
+          [sg.Button('Update', auto_size_button=False, size=(15,2)), sg.Button('Show Data', auto_size_button=False, size=(15,2)), sg.Button('Quit', auto_size_button=False, size=(15,2))]]
+
+
+# Create the window
+window = sg.Window('Window Title', layout, finalize=True)
+window.Size = (400,230)
+
+# Display and interact with the Window using an Event Loop
+while True:
+    event, values = window.read()
+    # See if user wants to quit or window was closed
+    if event == sg.WINDOW_CLOSED or event == 'Quit':
+        break
+    # Output a message to the window
+    elif event == "Show Data":
+        myData = ViewDb.ProcessData(dataDict["result"])
+        ViewDb.PlotDbData(myData[0], myData[1], myData[2])
+        continue
+    else:
+        continue
+    # Finish up by removing from the screen
+    window.close()
--- ViewDb.py
+++ ViewDb.py
@@ -19,6 +19,8 @@ def getDbData():
         configObj = json.load(file)
 
     engineString = f'postgresql+psycopg2://{configObj["userName"]}:{configObj["password"]}@{configObj["hostName"]}/{configObj["databaseName"]}'
+    
+    # create engine to connect
     engine = create_engine(engineString)
 
     #fetch data from DB
@@ -42,41 +44,43 @@ def ProcessData(queryResult):
     df30 = df[-30:]
     return (df, dfMax, df30)
 
-#Get and process data
 
-DataDict = getDbData()
-maxDate = DataDict["maxDate"]
-dfList = ProcessData(DataDict["result"])
-df = dfList[0]
-dfMax = dfList[1]
-df30 = dfList[2]
-
-#Process data for plotting
-myData = []
-for val in dfMax[["item_date","consumption"]].itertuples():
-    myData.append([val[1].strftime('%d.%m.%Y'),val[2]])
+def PlotDbData(df, dfMax, df30):
+    #Process data for plotting
+    myData = []
+    for val in dfMax[["item_date","consumption"]].itertuples():
+        myData.append([val[1].strftime('%d.%m.%Y'),val[2]])
 
 
-fig = plt.figure(figsize=(18,8))
-gs = fig.add_gridspec(2,2)
-ax1 = fig.add_subplot(gs[0,:])
-ax1.plot(df["item_date"], df["consumption"], label="Spotřeba")
-ax1.plot(df["item_date"], df["ma7"], label="7denní průměr")
-ax1.grid(axis="y", lw=0.5)
-ax1.set_xlabel("Datum")
-ax1.set_ylabel("kWh/den")
-ax1.set_title("Denní Spotřeba (kWh)")
-ax1.legend(loc=(0.01,0.78))
-ax2 = fig.add_subplot(gs[1,0])
-ax2.plot(df30["item_date"], df30["consumption"],color="green", label="Spotřeba")
-ax2.plot(df30["item_date"], df30["ma7"], color="red", label="7denní průměr")
-ax2.grid(axis="y", lw=0.5)
-ax2.set_title("Spotřeba posledních 30 dní")
-ax2.legend(loc=(0.8,0.8))
-ax3 = fig.add_subplot(gs[1,1])
-ax3.axis("tight")
-ax3.axis("off")
-ax3.table(cellText=myData, colLabels=["Date","Spotřeba (kWh)"], colLoc="right", colColours=["#3399FF","#3399FF"], loc='center')
-ax3.set_title("TOP 5 Spotřeba")
-plt.tight_layout()
-plt.show()
+    fig = plt.figure(figsize=(18,8))
+    gs = fig.add_gridspec(2,2)
+    ax1 = fig.add_subplot(gs[0,:])
+    ax1.plot(df["item_date"], df["consumption"], label="Spotřeba")
+    ax1.plot(df["item_date"], df["ma7"], label="7denní průměr")
+    ax1.grid(axis="y", lw=0.5)
+    ax1.set_xlabel("Datum")
+    ax1.set_ylabel("kWh/den")
+    ax1.set_title("Denní Spotřeba (kWh)")
+    ax1.legend(loc=(0.01,0.78))
+    ax2 = fig.add_subplot(gs[1,0])
+    ax2.plot(df30["item_date"], df30["consumption"],color="green", label="Spotřeba")
+    ax2.plot(df30["item_date"], df30["ma7"], color="red", label="7denní průměr")
+    ax2.grid(axis="y", lw=0.5)
+    ax2.set_title("Spotřeba posledních 30 dní")
+    ax2.legend(loc=(0.8,0.8))
+    ax3 = fig.add_subplot(gs[1,1])
+    ax3.axis("tight")
+    ax3.axis("off")
+    ax3.table(cellText=myData, colLabels=["Date","Spotřeba (kWh)"], colLoc="right", colColours=["#3399FF","#3399FF"], loc='center')
+    ax3.set_title("TOP 5 Spotřeba")
+    plt.tight_layout()
+    plt.show()
+    return
+
+# DataDict = getDbData()
+# maxDate = DataDict["maxDate"]
+# dfList = ProcessData(DataDict["result"])
+# df = dfList[0]
+# dfMax = dfList[1]
+# df30 = dfList[2]
+# PlotDbData(df,dfMax,df30)
